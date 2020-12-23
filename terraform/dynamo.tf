a62c11b2e2c2c748b78836df71251df6df78f388
DeltaByte <thedeltabyte@gmail.com>
2020-12-23 23:07:22 +0000

    use workspace prefix on table name

--- terraform/dynamo.tf
+++ terraform/dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "companies" {
-  name         = "companies"
+  name         = "${terraform.workspace}-companies"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
