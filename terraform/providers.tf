40b4896283f6d7f3784399e2e34e412c2bf09859
DeltaByte <thedeltabyte@gmail.com>
2020-12-23 23:08:17 +0000

    move to new TF backend and AWS region

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-mediacodex"
+    bucket         = "mediacodex-dev-terraform-state"
     key            = "companies.tfstate"
-    region         = "eu-central-1"
+    region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock"
-    role_arn       = "arn:aws:iam::939514526661:role/remotestate/companies"
-    session_name   = "terraform"
+    dynamodb_table = "dev-terraform-lock"
   }
 }
 
@@ -27,24 +25,12 @@ variable "deploy_aws_accounts" {
 }
 
 provider "aws" {
-  region              = "eu-central-1"
+  region              = "us-east-1"
   allowed_account_ids = var.deploy_aws_accounts[local.environment]
   assume_role {
     role_arn = var.deploy_aws_roles[local.environment]
   }
 }
+
 data "aws_region" "current" {}
 data "aws_caller_identity" "current" {}
-
-data "terraform_remote_state" "core" {
-  backend   = "s3"
-  workspace = terraform.workspace
-  config = {
-    bucket       = "terraform-state-mediacodex"
-    key          = "core.tfstate"
-    region       = "eu-central-1"
-    role_arn     = "arn:aws:iam::939514526661:role/remotestate/website"
-    session_name = "terraform"
-  }
-}
-
